1134fa1560dce0e9962cddfde4af38fd0b2573e1
Jad Dina <jad.dina19@gmail.com>
2025-10-26 22:41:09 -0600

    started the state machine exercise

--- app/build/zephyr/isr_tables.c
+++ app/build/zephyr/isr_tables.c
@@ -58,13 +58,13 @@ const uintptr_t __irq_vector_table _irq_vector_table[48] = {
 	((uintptr_t)&_isr_wrapper),
 };
 const struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
-	{(const void *)0x2219, (ISR)0x476d}, /* 0 */
+	{(const void *)0x22c9, (ISR)0x4877}, /* 0 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 1 */
-	{(const void *)0x4d4c, (ISR)0x46d1}, /* 2 */
+	{(const void *)0x4e54, (ISR)0x47db}, /* 2 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 3 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 4 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 5 */
-	{(const void *)0x28b9, (ISR)0x476d}, /* 6 */
+	{(const void *)0x2969, (ISR)0x4877}, /* 6 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 7 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 8 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 9 */
@@ -75,7 +75,7 @@ const struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 14 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 15 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 16 */
-	{(const void *)0x0, (ISR)0x1a19}, /* 17 */
+	{(const void *)0x0, (ISR)0x1a55}, /* 17 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 18 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 19 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 20 */
--- app/src/my_state_machine.c
+++ app/src/my_state_machine.c
@@ -5,65 +5,120 @@
  #include <zephyr/smf.h>
 
  #include "LED.h"
+ #include "BTN.h"
  #include "my_state_machine.h"
 
+ static void s0_state_entry(void* o);
+ static enum smf_state_result state_0(void* o);
 
- static void led_on_state_entry(void* o);
- static enum smf_state_result led_on_state_run(void* o);
- static void led_off_state_entry(void* o);
- static enum smf_state_result led_off_state_run(void* o); 
+ static void s1_state_entry(void* o);
+ static enum smf_state_result state_1(void* o);
+
+ static void s2_state_entry(void* o);
+ static enum smf_state_result state_2(void* o);
+
+ static void s3_state_entry(void* o);
+ static enum smf_state_result state_3(void* o);
+
+ static void s4_state_entry(void* o);
+ static enum smf_state_result state_4(void* o);
+
 
  enum led_state_machine_states{
-    LED_ON_STATE,
-    LED_OFF_STATE
+    S0,
+    S1,
+    S2,
+    S3,
+    S4
  };
 
  typedef struct{
     struct smf_ctx ctx;
     uint16_t count;
+    enum btn_id_t btn;
  }led_state_object_t;
 
  static const struct smf_state led_states[] = {
-    [LED_ON_STATE] = SMF_CREATE_STATE(led_on_state_entry, led_on_state_run, NULL, NULL, NULL),
-    [LED_OFF_STATE] = SMF_CREATE_STATE(led_off_state_entry, led_off_state_run, NULL, NULL, NULL)    
+    [S0] = SMF_CREATE_STATE(s0_state_entry, state_0, NULL, NULL, NULL),
+    [S1] = SMF_CREATE_STATE(s1_state_entry, state_1, NULL, NULL, NULL),
  };
 
  static led_state_object_t led_state_object;
 
+ void button_pressed(){
+   if(BTN_is_pressed(BTN0)){
+      led_state_object.btn = BTN0;
+   }
+   else if(BTN_is_pressed(BTN1)){
+      led_state_object.btn = BTN1;
+   }
+   else if(BTN_is_pressed(BTN2)){
+      led_state_object.btn = BTN2;
+   }
+   else if(BTN_is_pressed(BTN3)){
+      led_state_object.btn = BTN3;
+   }
+ }
+
  void state_machine_init(){
+   printk("state 0");
     led_state_object.count = 0;
-    smf_set_initial(SMF_CTX(&led_state_object), &led_states[LED_ON_STATE]);
+    smf_set_initial(SMF_CTX(&led_state_object), &led_states[S0]);
  }
 
  int state_machine_run(){
     return smf_run_state(SMF_CTX(&led_state_object));
  }
-static void led_on_state_entry(void* o){
-    LED_set(LED0, LED_ON);
+
+ static void s0_state_entry(void* o){
+   LED_set(LED0, LED_OFF);
+   LED_set(LED1, LED_OFF);
+   LED_set(LED2, LED_OFF);
+   LED_set(LED3, LED_OFF);
+ }
+ 
+ static enum smf_state_result state_0(void* o){
+      if(led_state_object.btn == BTN0){
+         printk("state 1");
+         led_state_object.count = 0;
+         smf_set_state(SMF_CTX(&led_state_object), &led_states[S1]);
+      }
+      else{
+         button_pressed();
+         led_state_object.count++;
+      }
+      return SMF_EVENT_HANDLED;
+ }
+
+static void s1_state_entry(void *o){
+   LED_blink(LED0, LED_4HZ);
 }
 
-static enum smf_state_result led_on_state_run(void* o){
-    if(led_state_object.count > 500) {
-        led_state_object.count = 0;
-        smf_set_state(SMF_CTX(&led_state_object), &led_states[LED_OFF_STATE]);
-    }else{
-        led_state_object.count++;
-    }
+ static enum smf_state_result state_1(void* o){
+   if(led_state_object.btn == BTN2){
+     led_state_object.count = 0;
+      smf_set_state(SMF_CTX(&led_state_object), &led_states[S4]); 
+   }
+   else if(led_state_object.btn == BTN1){
+      led_state_object.count = 0;
+      smf_set_state(SMF_CTX(&led_state_object), &led_states[S2]); 
+   }
+   else if(led_state_object.btn == BTN3){
+      led_state_object.count = 0;
+      smf_set_state(SMF_CTX(&led_state_object), &led_states[S0]); 
+   }
+   else{
+      button_pressed();
+      led_state_object.count++;
+   }
+   return SMF_EVENT_HANDLED;
+ }
 
-    return SMF_EVENT_HANDLED;
-}
-static void led_off_state_entry(void* o){
-    LED_set(LED0, LED_OFF);
+ static void s1_state_entry(void *o){
+   LED_set(LED0, LED_ON);
+   LED_set(LED2, LED_ON);
+   LED_set(LED3, LED_OFF);
+   LED_set(LED1, LED_OFF);
 }
 
-static enum smf_state_result led_off_state_run(void* o){
-    if(led_state_object.count > 500) {
-        led_state_object.count = 0;
-        smf_set_state(SMF_CTX(&led_state_object), &led_states[LED_ON_STATE]);
-    }else{
-        led_state_object.count++;
-    }
-
-    return SMF_EVENT_HANDLED;
-}
   
