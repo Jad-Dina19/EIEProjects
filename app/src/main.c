1a8c08a57d747057eea545192d0a0a449fe0194b
Jad Dina <jad.dina19@gmail.com>
2025-10-30 19:08:28 -0600

    did the pwm demonstratiion

--- app/src/main.c
+++ app/src/main.c
@@ -2,15 +2,14 @@
  * main.c
  */
 
-#include <inttypes.h>
-
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <inttypes.h>
 
 #include "BTN.h"
 #include "LED.h"
 
-#define SLEEP_MS 1
+#define SLEEP_MS_TIME 1
 
 int main(void) {
 
@@ -21,8 +20,16 @@ int main(void) {
     return 0;
   }
 
+  uint8_t current_duty_cycle = 0;
+  LED_pwm(LED0, current_duty_cycle);
+
   while(1) {
-    k_msleep(SLEEP_MS);
+    if(BTN_check_clear_pressed(BTN0)){
+      current_duty_cycle = (current_duty_cycle >= 100) ? 0 : (current_duty_cycle + 10);
+      printk("Setting LED0 to %d%% brightness.\n", current_duty_cycle);
+      LED_pwm(LED0, current_duty_cycle); 
+    }
+    k_msleep(SLEEP_MS_TIME);
   }
 	return 0;
 }
